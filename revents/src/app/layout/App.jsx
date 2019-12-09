8060c5b12f312fa55cc038d89e3027d31a1b3758
Dawid <budzynski.dawid@gmail.com>
2019-12-09 21:27:46 +0100

    EventDachboard initial

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import EventDachboard from "../../features/event/EventDashboard/EventDachboard";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-vents</h1>
-        <Button icon="smile" content="React Button" />
+        <EventDachboard />
       </div>
     );
   }
