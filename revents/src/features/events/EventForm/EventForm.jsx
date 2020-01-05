088f6bee215b00bb010c8695e93dec70b875ba08
Dawid Budzynski <budzynski.dawid@gmail.com>
2020-01-05 18:39:37 +0100

    changes to Create Event form

--- revents/src/features/events/EventForm/EventForm.jsx
+++ revents/src/features/events/EventForm/EventForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Segment, Form, Button } from "semantic-ui-react";
+import { createEvent, updateEvent } from "../eventActions";
+import cuid from "cuid";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -22,6 +24,11 @@ const mapState = (state, ownProps) => {
   };
 };
 
+const actions = {
+  createEvent,
+  updateEvent
+};
+
 class EventForm extends Component {
   state = { ...this.props.event };
 
@@ -37,8 +44,15 @@ class EventForm extends Component {
     evt.preventDefault();
     if (this.state.id) {
       this.props.updateEvent(this.state);
+      this.props.history.push(`/events/${this.state.id}`);
     } else {
-      this.props.createEvent(this.state);
+      const newEvent = {
+        ...this.state,
+        id: cuid(),
+        hostPhotoURL: "assets/user.png"
+      };
+      this.props.createEvent(newEvent);
+      this.props.history.push(`/events`);
     }
   };
 
@@ -111,4 +125,4 @@ class EventForm extends Component {
   }
 }
 
-export default connect(mapState)(EventForm);
+export default connect(mapState, actions)(EventForm);
